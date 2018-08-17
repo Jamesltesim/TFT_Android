6365857f88a55a2c3dd029c5d28365c1315bbefe
james <james.ltesim@gmail.com>
2018-08-17 16:30:54 +0800

    添加 me

--- app/src/main/java/com/example/james/tft_android/me/MeFragment.java
+++ app/src/main/java/com/example/james/tft_android/me/MeFragment.java
@@ -1,5 +1,6 @@
 package com.example.james.tft_android.me;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,15 +8,33 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.james.tft_android.R;
 
 public class MeFragment extends Fragment {
 
+    private View rootView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_me,container,false);
+        rootView = inflater.inflate(R.layout.fragment_me,container,false);
         return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView tv = rootView.findViewById(R.id.textView);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),TestActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
