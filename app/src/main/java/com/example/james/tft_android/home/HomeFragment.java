7a888b0acddc50379d1433016d263b0e5a3f9a43
james <james.ltesim@gmail.com>
2018-08-17 16:18:17 +0800

    菜单 UI

--- app/src/main/java/com/example/james/tft_android/home/HomeFragment.java
+++ app/src/main/java/com/example/james/tft_android/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.james.tft_android.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,18 +8,40 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.james.tft_android.R;
+import com.example.james.tft_android.home.menu.MenuListActivity;
 
 public class HomeFragment extends Fragment {
 
+    private View rootView;
+//    protected
     public HomeFragment() {
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home,container,false);
+        rootView = inflater.inflate(R.layout.fragment_home,container,false);
         return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        TextView tv = rootView.findViewById(R.id.textView);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MenuListActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
     }
 }
